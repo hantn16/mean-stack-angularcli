aa2d4bba632462202a2ee264209b76354ecc1554
Han Trinh <hantn16@gmail.com>
2018-05-14 11:23:50 +0700

    Ghep giao dien 1

--- src/app/main/main.module.ts
+++ src/app/main/main.module.ts
@@ -3,12 +3,17 @@ import { CommonModule } from '@angular/common';
 
 import { MainRoutingModule } from './main-routing.module';
 import { MainComponent } from './main.component';
+import { SidebarMenuComponent } from '../shared/sidebar-menu/sidebar-menu.component';
+import { TopMenuComponent } from '../shared/top-menu/top-menu.component';
 
 @NgModule({
   imports: [
     CommonModule,
     MainRoutingModule
   ],
-  declarations: [MainComponent]
+  declarations: [MainComponent,SidebarMenuComponent,TopMenuComponent],
+  exports: [
+    SidebarMenuComponent,TopMenuComponent
+  ]
 })
 export class MainModule { }
