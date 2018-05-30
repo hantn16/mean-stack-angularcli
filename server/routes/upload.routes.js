956899c28a1bd04113a9ed9a5641dceeef6127b0
Han Trinh <hantn16@gmail.com>
2018-05-30 11:26:56 +0700

    Build upload api 1.2

--- server/routes/upload.routes.js
+++ server/routes/upload.routes.js
@@ -5,8 +5,8 @@ const multer = require('multer');
 const path = require('path');
 const Loki = require('lokijs');
 const fs = require('fs');
-const {authenticate}  = require('../middleware/authenticate');
-const {upload,db} = require('../configs/multer.config');
+const { authenticate } = require('../middleware/authenticate');
+const { upload, db } = require('../configs/multer.config');
 
 const loadCollection = function (colName, db) {
     return new Promise(resolve => {
@@ -17,11 +17,38 @@ const loadCollection = function (colName, db) {
     });
 }
 
-router.post('/profile', upload.single('avatar'), (req, res) => {
+router.post('/image', upload.single('avatar'), (req, res) => {
     loadCollection('images', db).then((collection) => {
         const data = collection.insert(req.file);
         db.saveDatabase();
         res.send({ id: data.$loki, fileName: data.filename, originalName: data.originalname });
     }).catch((e) => { res.sendStatus(400).send(e); });
+})
+
+//upload multi-images
+router.post('/images', upload.array('photos', 12), (req, res) => {
+    loadCollection('images', db).then((col) => {
+        let data = [].concat(col.insert(req.files));
+        db.saveDatabase();
+        res.send(data.map(x => ({ id: x.$loki, fileName: x.filename, originalName: x.originalname })));
+    }).catch((e) => res.sendStatus(400).send(e));
+})
+
+//get all images
+router.get('/images', (req, res) => {
+    loadCollection('images', db).then((col) => {
+        res.send(col.data);
+    }).catch((err) => res.sendStatus(400).send(err));
+})
+
+router.get('/images/:id', (req, res) => {
+    loadCollection('images', db).then((col) =>{
+        const result = col.get(req.params.id);
+        if (!result) {
+            return res.sendStatus(404);
+        };
+        res.setHeader('Content-Type', result.mimetype);
+        fs.createReadStream(path.join(UPLOAD_PATH, result.filename)).pipe(res);
+    }).catch(e => res.sendStatus(400).send(e));  
 })
 module.exports = router;
